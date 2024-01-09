a403bb422b4d42d522d734099a237011b93940b7
TomSidi07 <toumanisidibe2@gmail.com>
2024-01-09 18:06:42 +0000

    Message, User, and SUBMIT Model created

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,13 +1,17 @@
-import { User, Message } from "../models/model.js";
+import { User, Message, SUBMIT } from "../models/model.js";
 import MessageView from "../views/MessageView.js";
 
-let tom = new User("Tom", "Sidibe");
 let logMessage = (eve) => {
-  eve.preventDefault();
-
-  console.log(tom);
-  //   let newMsg = new Message();
-  console.log("ok");
+  try {
+    eve.preventDefault();
+    let tom = new User("Tom", "Sidibe");
+    let content = "Hello im tom";
+    let msg = new Message(tom, content);
+    let submit = new SUBMIT(tom, msg);
+    console.log(msg);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 let init = () => {
--- models/model.js
+++ models/model.js
@@ -3,23 +3,41 @@ export class User {
   firstName;
   lastName;
   constructor(firstName, lastName) {
-    this.id =
-      firstName.slice(0, 3) +
-      lastName.slice(0, 3) +
-      Math.round(Math.random() * 100 + 1);
-    this.firstName = firstName;
-    this.lastName = lastName;
+    try {
+      if (
+        typeof lastName == "string" &&
+        typeof firstName == "string" &&
+        lastName.length >= 3 &&
+        firstName.length >= 3
+      ) {
+        this.id =
+          firstName.slice(0, 3) +
+          lastName.slice(0, 3) +
+          Math.round(Math.random() * 100 + 1);
+        this.firstName = firstName;
+        this.lastName = lastName;
+      } else
+        throw new Error(
+          "Your name and your lastname must be at least 3 caracter length"
+        );
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 export class Message {
-  id;
-  username;
-  content;
-  constructor(username, content) {
-    this.id = User.id;
+  constructor(submitter, content) {
+    this.id =
+      submitter.id.slice(3) + 0 + 0 + Math.floor(Math.random() * 100) ** 5;
+    this.submitter = submitter;
+    this.content = content;
   }
 }
-export let SUBMIT = {
-  // user: this.user,
-  // message: this.message,
-};
+export class SUBMIT {
+  user;
+  message;
+  constructor(user, message) {
+    this.message = message;
+    this.user = user;
+  }
+}
